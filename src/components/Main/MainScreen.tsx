0f845f1382b8196ce9315dff7ca1b0dc1378ffa7
illia <fialkivskiji@gmail.com>
2024-07-17 19:20:42 +0300

    fixes

--- src/components/Main/MainScreen.tsx
+++ src/components/Main/MainScreen.tsx
@@ -3,10 +3,8 @@ import styles from './MainScreen.module.css';
 import ProgressBar from './ProgressBar/ProgressBar';
 import ClickButton from './ClickButton/ClickButton';
 import TotalCoins from './TopBar/TotalCoins';
-import { useAppSelector } from 'src/hooks/useRedux';
 
 const MainScreen = () => {
-	const coins = useAppSelector((state) => state.coins.count);
 	return (
 		<div className={styles.container}>
 			<TotalCoins />
